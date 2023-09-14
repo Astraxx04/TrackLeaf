fc378deabb54ac9da1681dfa05ed7259f65a4837
Astraxx04 <gagan200254@gmail.com>
2023-09-14 19:42:39 +0530

    Initialisation

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();  
+require("dotenv").config();
+require("express-async-errors");
+const connectDB = require("./database/connect");
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+const startServer = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        console.log("Database connection established successfully...");
+        app.listen(port, console.log(`Server started on port ${port}...`));
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+startServer();
