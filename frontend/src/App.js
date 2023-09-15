affb34f983bc734282d352632b977c73707c1333
heisdinesh <dinesh35382@gmail.com>
2023-09-15 07:10:49 +0530

    frontend basic setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Landing, QRGenerator } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      Hello Bytches
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/QRGenerator" element={<QRGenerator />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Sidebar, Navbar } from "../components";
+
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
