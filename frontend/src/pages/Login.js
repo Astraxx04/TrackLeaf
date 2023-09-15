3c4ab3437e56741b4945cc8b1192e94d234ea894
aazaleas <fierypitsofhello@gmail.com>
2023-09-15 19:42:12 +0530

    asdsfzdf

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -21,8 +21,8 @@ const Login = () => {
 
     return (
         <div className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center">
-            <div className="bg-gray-100 shadow-lg rounded-md mb-10 p-8 h-2/3 w-2/6">
-                <h2 className="text-4xl font-semibold mb-10">Login</h2>
+            <div className="bg-gray-100 shadow-lg rounded-md mb-10 p-8 h-2/3 w-1/4">
+                <h2 className="text-4xl font-semibold mb-14">Login</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         {/* <label htmlFor="username" className="block text-gray-700 font-medium mb-2">
@@ -34,7 +34,7 @@ const Login = () => {
                             name="username"
                             value={username}
                             onChange={handleUsernameChange}
-                            className="w-full px-4 py-2 border text-lg rounded-md focus:outline-none focus:border-primary"
+                            className="w-full px-4 py-2 border text-lg hover:border-primary rounded-md focus:outline-none focus:border-primary"
                             placeholder="Enter your username"
                             required
                         />
@@ -49,13 +49,13 @@ const Login = () => {
                             name="password"
                             value={password}
                             onChange={handlePasswordChange}
-                            className="w-full px-4 py-2 border rounded-md text-lg mb-10 focus:outline-none focus:border-primary"
+                            className="w-full px-4 py-2 border rounded-md text-lg mb-10 hover:border-primary focus:outline-none focus:border-primary"
                             placeholder="Enter your password"
                             required
                         />
                     </div>
                     <div className="text-center">
-                        <button type="submit" className="w-full bg-primary text-white rounded-md px-4 py-2 text-2xl hover:bg-opacity-80">
+                        <button type="submit" className="w-full bg-primary text-white rounded-md px-4 py-2 text-xl hover:bg-opacity-80">
                             Log In
                         </button>
                     </div>
--- frontend/src/pages/QRGenerator.js
+++ frontend/src/pages/QRGenerator.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { jsPDF } from 'jspdf';
+import axios from "axios"
 
 const QRGenerator = ({ hexImages }) => {
   const PAGE_WIDTH = 210;
@@ -41,11 +42,14 @@ const QRGenerator = ({ hexImages }) => {
   };
 
   const [number, setNumber] =  useState(0);
+  const [data, setData] = useState([])
 
 
   const handleGenerate = (e) => {
     e.preventDefault();
-    console.log(number);
+    axios.post("http://localhost:5000/api/v1/noofQrCodes")
+    .then((res)=>console.log(res))
+    .catch((err)=>console.log(err))
   };
 
   const hexToDataURL = (hex) => {
@@ -62,20 +66,22 @@ const QRGenerator = ({ hexImages }) => {
         <input className='w-3/4 rounded-md focus:border-primary border-0 py-2 px-2 hover:border-2 hover:border-primary' onChange = { (e) =>  setNumber(e.target.value)} type='text' id='noQrCodes' placeholder='Enter the number of QR Codes to be generated' />
         <button className='px-4 ml-4 py-2 text-white bg-primary rounded-md text-sm md:text-xl' onClick={handleGenerate}>Generate</button>
       </div>
-      {/* <div className='bg-white rounded-lg py-6 px-4'>
-        <div className='flex flex-wrap gap-4'>
-          {hexImages.map((hex, index) => (
-            <img
-              className='h-32 w-32 object-cover'
-              src={hexToDataURL(hex)}
-              alt={`Image ${index}`}
-            />
-          ))}
-        </div>
-        <div className='text-lg text-white'>
-          <button className='px-16 py-4 bg-primary rounded-md font-semibold' onClick={handlePrintClick}>Print</button>
-        </div>
-      </div> */}
+{
+  data &&       <div className='bg-white rounded-lg py-6 px-4'>
+  <div className='flex flex-wrap gap-4'>
+    {data.map((hex, index) => (
+      <img
+        className='h-32 w-32 object-cover'
+        src={hexToDataURL(hex)}
+        alt={`Image ${index}`}
+      />
+    ))}
+  </div>
+  <div className='text-lg text-white'>
+    <button className='px-16 py-4 bg-primary rounded-md font-semibold' onClick={handlePrintClick}>Print</button>
+  </div>
+</div>
+}
     </div>
   );
 };
