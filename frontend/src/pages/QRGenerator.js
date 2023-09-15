c040727b84530dca6383384967ab148b69960eeb
aazaleas <fierypitsofhello@gmail.com>
2023-09-15 16:26:05 +0530

    input no of QR

--- frontend/src/pages/QRGenerator.js
+++ frontend/src/pages/QRGenerator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { jsPDF } from 'jspdf';
 
 const QRGenerator = ({ hexImages }) => {
@@ -40,28 +40,44 @@ const QRGenerator = ({ hexImages }) => {
     doc.save('QR.pdf');
   };
 
+  const [number, setNumber] =  useState(0);
+
+
+  const handleGenerate = (e) => {
+    e.preventDefault();
+    console.log(number);
+
+  };
+
   const hexToDataURL = (hex) => {
     const cleanedHex = hex.startsWith('0x') ? hex.slice(2) : hex;
     return `data:image/png;base64,${cleanedHex}`;
   };
 
   return (
-
-    <div className='bg-white rounded-lg py-6 px-4'>
-      <div className='flex flex-wrap gap-4'>
-        {hexImages.map((hex, index) => (
-          <img
-            className='h-32 w-32 object-cover'
-            src={hexToDataURL(hex)}
-            alt={`Image ${index}`}
-          />
-        ))}
+    <div className="flex flex-col gap-4">
+      <div className="bg-white rounded-lg py-6 px-4  ">
+        <p className="text-start text-3xl font-bold">Generate QR</p>
       </div>
-      <div className='text-lg text-white'> 
-        <button className='px-16 py-4 bg-primary rounded-md font-semibold' onClick={handlePrintClick}>Print</button>
+      <div className= "bg-white flex flex-justify rounded-lg py-6 px-4  ">
+        <input className='w-3/4 rounded-md focus:border-primary border-0 py-2 px-2 hover:border-2 hover:border-primary' onChange = { (e) =>  setNumber(e.target.value)} type='text' id='noQrCodes' placeholder='Enter the number of QR Codes to be generated' />
+        <button className='px-4 ml-4 py-2 text-white bg-primary rounded-md text-xl' onClick={handleGenerate}>Generate</button>
       </div>
+      {/* <div className='bg-white rounded-lg py-6 px-4'>
+        <div className='flex flex-wrap gap-4'>
+          {hexImages.map((hex, index) => (
+            <img
+              className='h-32 w-32 object-cover'
+              src={hexToDataURL(hex)}
+              alt={`Image ${index}`}
+            />
+          ))}
+        </div>
+        <div className='text-lg text-white'>
+          <button className='px-16 py-4 bg-primary rounded-md font-semibold' onClick={handlePrintClick}>Print</button>
+        </div>
+      </div> */}
     </div>
-
   );
 };
 
